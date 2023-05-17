0048cfa3d81db8f969074b744bfbaaa3b07402f0
afonso <leonardojeanarg@gmail.com>
2023-05-17 20:50:10 -0300

    Text, Buttons and copied images added.

--- app/page.js
+++ app/page.js
@@ -1,9 +1,34 @@
+/* import { CeoImg } from "@/components/CeoImg"; --*/
 import { Header } from "/components/header";
 import { Button } from "@/components/Button";
+import Image from "next/image";
 export default function Home() {
   return (
     <>
       <Header />
+      <div style={{ display: "flex" }}>
+        <div className="containerText">
+          <h1>
+            A better way <br></br>to create
+          </h1>
+          <p>
+            Bring your creative vision to life as you stream <br></br> work
+            sessions in HD, video chat whit your team <br></br>and collaborate
+            live - no matter where you are.
+          </p>
+          <div className="buttonText">
+            <Button isTransparent={false} children={"GET STARTED"} />
+            <Button isTransparent={true} children={"WATCH DEMO"} />
+          </div>
+        </div>
+        <Image
+          className="imagemLateral"
+          src="/fotoLateral.png"
+          width={535}
+          height={535}
+          alt="foto dos idealizadores"
+        />
+      </div>
     </>
   );
 }
---
+++ components/CeoImg/index.js
@@ -0,0 +1,30 @@
+/*
+import styles from "../styles.module.css";
+
+export function CeoImg({ isAfonso, isLaurentino, isBia, children }) {
+  let fotoAfonso = "/afonso.png";
+  let fotoBia = "/bia.png";
+  let fotoLaurentino = "/laurentino.png";
+
+  return (
+    <>
+      <img
+        src={
+          isAfonso
+            ? fotoAfonso
+            : isLaurentino
+            ? fotoLaurentino
+            : isBia
+            ? fotoBia
+            : null
+        }
+        className={`${styles.CeoImg}
+        ${isAfonso === true ? fotoAfonso : null}
+        ${isLaurentino === true ? fotoLaurentino : null}
+        ${isBia === true ? fotoBia : null} 
+        `}
+      />
+    </>
+  );
+}
+*/
